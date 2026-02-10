d478867176357836f546ca5ef96ee59846e2aaaf
juegos2 <brithanycedeno12@gmail.com>
2026-02-10 13:03:12 -0500

    Delete js directory

--- js/loader.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loader = document.getElementById("loader");
-    const contenido = document.getElementById("contenido");
-
-    setTimeout(() => {
-        loader.style.display = "none";
-        contenido.style.display = "block";
-    }, 600);
-});
